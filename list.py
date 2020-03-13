4a7b23ffe67bb3e3aeb2a494304019e2aefc14fa
Michael Pandolfelli <50030810+mikepando@users.noreply.github.com>
2020-03-13 14:54:39 -0400

    email dictionary

---
+++ list.py
@@ -0,0 +1,5 @@
+users={'John Doe':'johndoe@email.com',
+         'Joe Schmoe':'default@hotmail.com',
+         'Jane Doe':'jane1234doe@aol.com',
+         'Shane McConkey':'saucer.boy@legend.com'
+         }
