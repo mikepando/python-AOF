1644cbd93895608656abb7884dbacc3b270cf3d3
Michael Pandolfelli <50030810+mikepando@users.noreply.github.com>
2021-07-20 11:58:52 -0400

    Delete list.py
    No longer needed as the email list is now an Excel spreadsheet.

--- list.py
+++
@@ -1,5 +0,0 @@
-users={'John Doe':'johndoe@email.com',
-         'Joe Schmoe':'default@hotmail.com',
-         'Jane Doe':'jane1234doe@aol.com',
-         'Shane McConkey':'saucer.boy@legend.com'
-         }
